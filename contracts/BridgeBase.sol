386678fe4540d84bfff50ed46bfead67955c57d8
umaresso <umarkhatab467@gmail.com>
2023-02-12 11:54:03 +0500

    initial commit

---
+++ contracts/BridgeBase.sol
@@ -0,0 +1,116 @@
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SwapBridgeBase {
+    IERC20 token;
+    mapping(address => mapping(bytes => bool)) processedUserSignatures;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    event DepositSuccess(
+        address user,
+        uint amount,
+        uint nonce,
+        bytes signature
+    );
+    event WithdrawSuccess(
+        address user,
+        uint amount,
+        uint nonce,
+        bytes signature
+    );
+
+    function swap(
+        address user,
+        uint amount,
+        uint nonce,
+        bytes memory signature
+    ) public {
+        bytes32 message = prefixed(
+            keccak256(abi.encodePacked(user, amount, nonce))
+        );
+        require(
+            recoverSigner(message, signature) == user,
+            "Signature Error: Not Signed by the Proposer"
+        );
+
+        require(
+            token.allowance(msg.sender, address(this)) >= amount,
+            "Insufficient Allowance to contract"
+        );
+        // take tokens in from user
+        token.transferFrom(user, address(this), amount);
+
+        emit DepositSuccess(user, amount, nonce, signature);
+    }
+
+    function withdraw(
+        address user,
+        uint amount,
+        uint nonce,
+        bytes memory signature
+    ) public {
+        // if the signature is valid
+        bytes32 message = prefixed(
+            keccak256(abi.encodePacked(user, amount, nonce))
+        );
+        require(
+            recoverSigner(message, signature) == user,
+            "Signature Error: Not Signed by the Proposer"
+        );
+        require(
+            !processedUserSignatures[user][signature],
+            "Transaction has already been processed!"
+        );
+        token.transferFrom(address(this), user, amount);
+        processedUserSignatures[user][signature] = true;
+
+        emit WithdrawSuccess(user, amount, nonce, signature);
+    }
+
+    /** Signature Verification Utilities */
+
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+            );
+    }
+
+    function recoverSigner(
+        bytes32 message,
+        bytes memory sig
+    ) internal pure returns (address) {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) internal pure returns (uint8, bytes32, bytes32) {
+        require(sig.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
+    }
+}
---
+++ scripts/eth-bsc-bridge.js
@@ -0,0 +1,127 @@
+const Web3 = require("web3");
+const BridgeEth = require("../build/contracts/BridgeEth.json");
+const BridgeBsc = require("../build/contracts/BridgeBsc.json");
+
+// Instantiating web3 object with the Ethereum network's WebSocket URL
+const web3Eth = new Web3(
+  "wss://goerli.infura.io/ws/v3/0e88431708fb4d219a28755bf50fb061"
+);
+
+// Instantiating web3 object with the Binance Smart Chain network's RPC URL
+const web3Bsc = new Web3("https://data-seed-prebsc-1-s3.binance.org:8545");
+
+// The private key of the wallet to be used as the admin address
+const adminPrivKey =
+  "20f59edb6af6f23b164d83f03175455b72a9d9fc514006a48cbea92c49101a8d";
+
+// Deriving the public address of the wallet using the private key
+const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
+
+// Instantiating the BridgeEth contract with its ABI and address
+const bridgeEth = new web3Eth.eth.Contract(
+  BridgeEth.abi,
+  BridgeEth.networks["5"].address
+);
+
+// Instantiating the BridgeBsc contract with its ABI and address
+const bridgeBsc = new web3Bsc.eth.Contract(
+  BridgeBsc.abi,
+  BridgeBsc.networks["97"].address
+);
+
+// Listening to Transfer events emitted by the BridgeEth contract
+console.log("Listening to the events....");
+
+//
+
+bridgeEth.events.DepositSuccess({ fromBlock: 0 }).on("data", async (event) => {
+  const { user, amount, nonce, signature } = event.returnValues;
+  console.log(`
+    ETH Deposit Success:
+    - ${user} Depoisted ${amount} tokens
+    - Signature ${signature}
+  `);
+
+  // initiate withdraw transaction
+  // Destructuring the values from the event
+  const tx = bridgeBsc.methods.withdraw(user, amount, nonce, signature);
+
+  // Getting the gas price and gas cost required for the method call
+  const [gasPrice, gasCost] = await Promise.all([
+    web3Bsc.eth.getGasPrice(),
+    tx.estimateGas({ from: admin }),
+  ]);
+
+  // Encoding the ABI of the method
+  const data = tx.encodeABI();
+
+  // Preparing the transaction data
+  const txData = {
+    from: admin,
+    to: bridgeBsc.options.address,
+    data,
+    gas: gasCost,
+    gasPrice,
+  };
+
+  // Sending the transaction to the Binance Smart Chain
+  const receipt = await web3Bsc.eth.sendTransaction(txData);
+  // Logging the transaction hash
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+});
+
+bridgeEth.events.WithdrawSuccess({ fromBlock: 0 }).on("data", async (event) => {
+  const { user, amount, nonce, signature } = event.returnValues;
+  console.log(`
+    ETH Withdraw Success:
+    - User ${user} Withdrawn ${amount} tokens
+    - Signature ${signature}
+  `);
+});
+
+// BSC Events Handling
+
+bridgeBsc.events.DepositSuccess({ fromBlock: 0 }).on("data", async (event) => {
+  const { user, amount, nonce, signature } = event.returnValues;
+  console.log(`
+    BSC Deposit Success:
+    - ${user} Depoisted ${amount} tokens
+    - Signature ${signature}
+  `);
+
+  // initiate withdraw transaction
+  // Destructuring the values from the event
+  const tx = bridgeBsc.methods.withdraw(user, amount, nonce, signature);
+
+  // Getting the gas price and gas cost required for the method call
+  const [gasPrice, gasCost] = await Promise.all([
+    web3Bsc.eth.getGasPrice(),
+    tx.estimateGas({ from: admin }),
+  ]);
+
+  // Encoding the ABI of the method
+  const data = tx.encodeABI();
+
+  // Preparing the transaction data
+  const txData = {
+    from: admin,
+    to: bridgeBsc.options.address,
+    data,
+    gas: gasCost,
+    gasPrice,
+  };
+
+  // Sending the transaction to the Binance Smart Chain
+  const receipt = await web3Bsc.eth.sendTransaction(txData);
+  // Logging the transaction hash
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+});
+
+bridgeBsc.events.WithdrawSuccess({ fromBlock: 0 }).on("data", async (event) => {
+  const { user, amount, nonce, signature } = event.returnValues;
+  console.log(`
+    BSC Withdraw Success:
+    - User ${user} Withdrawn ${amount} tokens
+    - Signature ${signature}
+  `);
+});
